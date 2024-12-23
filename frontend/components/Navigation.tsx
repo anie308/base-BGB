b7bc11a21d2d5db90225c11e322c1810d84da8f1
Soheima Maradiaga <soheimam@gmail.com>
2024-12-23 17:12:01 +0100

    init

---
+++ frontend/components/Navigation.tsx
@@ -0,0 +1,13 @@
+
+function Navigation() {
+    return (
+        <nav className="flex justify-between items-center p-4">
+            <div className="flex items-center">
+                {/* <Link href="/">Home</Link>
+                <Link href="/about">About</Link> */}
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
